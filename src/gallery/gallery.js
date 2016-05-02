5b10265373aa95e2fb1c2cb403118b202dd20e9a
Alina <asalisinasa@gmail.com>
2016-05-02 15:16:51 +0300

    created Gallery.prototype methods

--- src/gallery/gallery.js
+++ src/gallery/gallery.js
@@ -24,156 +24,166 @@ var picturesList = galleryElement.getElementsByTagName('IMG');
 
 /** @constructor */
 var Gallery = function() {
-  var self = this;
-  var pictureUrl;
   this.picturesSrc = [];
+  this.pictureNumber = 1;
 
+  this._onGalleryClick = this._onGalleryClick.bind(this);
+  this._onNextClick = this._onNextClick.bind(this);
+  this._onPrevClick = this._onPrevClick.bind(this);
+  this._onDocumentKeyDown = this._onDocumentKeyDown.bind(this);
+  this._onCloseClick = this._onCloseClick.bind(this);
 
-  this.getSrcArray = function() {
-    for (var i = 0; i < picturesList.length; i++) {
-      self.picturesSrc.push(picturesList[i].getAttribute('src'));
-      picturesList[i].id = i + 1;
-    }
-  };
-
-
-  this.createGallery = function() {
-    self.getSrcArray();
-    this.currentPicture = picturesContainer.appendChild(new Image());
-    picturesSum.textContent = picturesList.length;
-  };
-
-
-  this.changePicture = function(pictureNumb) {
-    self.pictureNumber = pictureNumb;
-    self.currentPicture.src = self.picturesSrc[self.pictureNumber - 1];
-    currentPictureNumber.textContent = self.pictureNumber;
-    self.setBtnDisabled();
-  };
-
-  this.changeURL = function(url) {
-    if(url) {
-      pictureUrl = '#photo/' + url;
-      location.hash = pictureUrl;
-    } else {
-      window.location.hash = '';
-    }
-  };
-
-
-  /** @param {Number} pictureNumb */
-  this.showGallery = function(picture) {
-    this.changePicture(picture);
-
-    utilities.showElem(galleryOverlay);
-
-    btnNext.addEventListener('click', self._onNextClick);
-    btnPrev.addEventListener('click', self._onPrevClick);
-    btnClose.addEventListener('click', self._onCloseClick);
-    window.addEventListener('keydown', self._onDocumentKeyDown);
-  };
-
-
-  /** @param {MouseEvent} evt */
-  this.onGalleryClick = function(evt) {
-    evt.preventDefault();
-    if (evt.target.tagName === 'IMG') {
-      self.changeURL(evt.target.getAttribute('src'));
-      self.pictureNumber = +evt.target.id;
-    }
-  };
-
-
-  this._onHashChange = function() {
-    var pictureRegExp = new RegExp(/#photo\/(\S+)/);
-    var checkHash = location.hash.match(pictureRegExp);
-    if (checkHash) {
-      self.pictureNumber = self.picturesSrc.indexOf(checkHash[1]) + 1;
-      self.showGallery(self.pictureNumber);
-    } else {
-      self.closeGallery();
-    }
-  };
-
-
-  /** @param {MouseEvent} evt */
-  this._onNextClick = function(evt) {
-    evt.preventDefault();
-    self.showNext();
-  };
-
-
-  /** @param {MouseEvent} evt */
-  this._onPrevClick = function(evt) {
-    evt.preventDefault();
-    self.showPrev();
-  };
-
-
-  this.showNext = function() {
-    self.pictureNumber++;
-    if(self.pictureNumber < self.picturesSrc.length + 1) {
-      self.changeURL(self.picturesSrc[self.pictureNumber - 1]);
-    }
-    self.setBtnDisabled();
-  };
-
-
-  this.showPrev = function() {
-    self.pictureNumber--;
-    if(self.pictureNumber > -1) {
-      self.changeURL(self.picturesSrc[self.pictureNumber - 1]);
-    }
-    self.setBtnDisabled();
-  };
-
-
-  this.setBtnDisabled = function() {
-    btnNext.classList.toggle('overlay-gallery-control-disabled', self.pictureNumber >= self.picturesSrc.length);
-    btnPrev.classList.toggle('overlay-gallery-control-disabled', self.pictureNumber <= 1);
-  };
-
-
-  this.closeGallery = function() {
-    history.pushState(null, null, window.location.pathname);
-
-    utilities.hideElem(galleryOverlay);
-
-    btnNext.removeEventListener('click', self._onNextClick);
-    btnPrev.removeEventListener('click', self._onPrevClick);
-    btnClose.removeEventListener('click', self._onCloseClick);
-    window.removeEventListener('keydown', self._onDocumentKeyDown);
-  };
-
-
-  this._onDocumentKeyDown = function(evt) {
-    evt.preventDefault();
-    switch (evt.keyCode) {
-      case utilities.KeyCode.ESC:
-        self.changeURL();
-        break;
-      case utilities.KeyCode.RIGHT:
-        self.showNext();
-        break;
-      case utilities.KeyCode.LEFT:
-        self.showPrev();
-        break;
-      default:
-        break;
-    }
-  };
-
-
-  /** @param {MouseEvent} evt */
-  this._onCloseClick = function(evt) {
-    evt.preventDefault();
-    self.changeURL();
-  };
-
-
-  this.createGallery();
+  this._createGallery();
   this._onHashChange();
-  window.addEventListener('hashchange', this._onHashChange);
+  window.addEventListener('hashchange', this._onHashChange.bind(this));
 };
+
+
+Gallery.prototype.getSrcArray = function() {
+  this.picturesSrc = Array.prototype.map.call(picturesList, function(picture) {
+    return picture.getAttribute('src');
+  });
+};
+
+
+Gallery.prototype._createGallery = function() {
+  this.getSrcArray();
+  this.currentPicture = picturesContainer.appendChild(new Image());
+  picturesSum.textContent = picturesList.length;
+};
+
+
+Gallery.prototype.changePicture = function(pictureNumb) {
+  this.pictureNumber = pictureNumb;
+  this.currentPicture.src = this.picturesSrc[this.pictureNumber - 1];
+  currentPictureNumber.textContent = this.pictureNumber;
+  this.setBtnDisabled();
+};
+
+
+/** @param {String} url */
+Gallery.prototype.changeURL = function(url) {
+  var pictureUrl;
+
+  if(url) {
+    pictureUrl = '#photo/' + url;
+    location.hash = pictureUrl;
+  } else {
+    window.location.hash = '';
+  }
+};
+
+
+Gallery.prototype.setBtnDisabled = function() {
+  btnNext.classList.toggle('overlay-gallery-control-disabled', this.pictureNumber >= this.picturesSrc.length);
+  btnPrev.classList.toggle('overlay-gallery-control-disabled', this.pictureNumber <= 1);
+};
+
+
+/** @param {Number} pictureNumb */
+Gallery.prototype.showGallery = function(picture) {
+  this.changePicture(picture);
+
+  utilities.showElem(galleryOverlay);
+
+  btnNext.addEventListener('click', this._onNextClick);
+  btnPrev.addEventListener('click', this._onPrevClick);
+  btnClose.addEventListener('click', this._onCloseClick);
+  window.addEventListener('keydown', this._onDocumentKeyDown);
+};
+
+
+/** @param {MouseEvent} evt */
+Gallery.prototype._onGalleryClick = function(evt) {
+  evt.preventDefault();
+  if (evt.target.tagName === 'IMG') {
+    this.changeURL(evt.target.getAttribute('src'));
+    this.pictureNumber = +evt.target.id;
+  }
+};
+
+
+Gallery.prototype._onHashChange = function() {
+  var PICTURE_REG_EXP = new RegExp(/#photo\/(\S+)/);
+  var checkHash = location.hash.match(PICTURE_REG_EXP);
+
+  if (checkHash) {
+    this.pictureNumber = this.picturesSrc.indexOf(checkHash[1]) + 1;
+    this.showGallery(this.pictureNumber);
+  } else {
+    this.closeGallery();
+  }
+};
+
+
+/** @param {MouseEvent} evt */
+Gallery.prototype._onNextClick = function(evt) {
+  evt.preventDefault();
+  this.showNext();
+};
+
+
+/** @param {MouseEvent} evt */
+Gallery.prototype._onPrevClick = function(evt) {
+  evt.preventDefault();
+  this.showPrev();
+};
+
+
+Gallery.prototype.showNext = function() {
+  this.pictureNumber++;
+  if(this.pictureNumber < this.picturesSrc.length + 1) {
+    this.changeURL(this.picturesSrc[this.pictureNumber - 1]);
+  }
+  this.setBtnDisabled();
+};
+
+
+Gallery.prototype.showPrev = function() {
+  this.pictureNumber--;
+  if(this.pictureNumber > -1) {
+    this.changeURL(this.picturesSrc[this.pictureNumber - 1]);
+  }
+  this.setBtnDisabled();
+};
+
+
+Gallery.prototype.closeGallery = function() {
+  history.pushState(null, null, window.location.pathname);
+
+  utilities.hideElem(galleryOverlay);
+
+  btnNext.removeEventListener('click', this._onNextClick);
+  btnPrev.removeEventListener('click', this._onPrevClick);
+  btnClose.removeEventListener('click', this._onCloseClick);
+  window.removeEventListener('keydown', this._onDocumentKeyDown);
+};
+
+
+/** @param {KeyboardEvent} evt */
+Gallery.prototype._onDocumentKeyDown = function(evt) {
+  evt.preventDefault();
+  switch (evt.keyCode) {
+    case utilities.KeyCode.ESC:
+      this.changeURL();
+      break;
+    case utilities.KeyCode.RIGHT:
+      this.showNext();
+      break;
+    case utilities.KeyCode.LEFT:
+      this.showPrev();
+      break;
+    default:
+      break;
+  }
+};
+
+
+/** @param {MouseEvent} evt */
+Gallery.prototype._onCloseClick = function(evt) {
+  evt.preventDefault();
+  this.changeURL();
+};
+
 
 module.exports = new Gallery();
